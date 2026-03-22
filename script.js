212369ffb79615b902e4e272b47c88637cbf85e3
NtouKe <255889853+NtouKe@users.noreply.github.com>
2026-03-22 16:09:12 +0200

    feat: implement addition logic and accumulator state
    feat: add operator handling and reset-on-input behavior

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const calculator = {
   currentInput: "0",
+  operator: null,
+  accumulator: null,
+  resetOnNextInput: false,
 
   display: {
     _upperEl: document.querySelector(".upper-display"),
     _lowerEl: document.querySelector(".lower-display"),
+    upperText: null,
+    updateUpper() {
+      this._upperEl.textContent = this.upperText;
+    },
+
     updateLower() {
       this._lowerEl.textContent = calculator.currentInput;
     },
   },
 
   handleDigit(number) {
+    if (this.resetOnNextInput) {
+      this.currentInput = "0";
+      this.resetOnNextInput = false;
+    }
     if (this.currentInput.length === 9) return;
     if (this.currentInput === "0") {
       this.currentInput = number;
@@ -19,10 +31,39 @@ const calculator = {
     this.display.updateLower();
   },
 
+  handelOperator(operator) {
+    operatorSymbols = {
+      addition: "+",
+      subtract: "-",
+      multiply: "x",
+      divide: "÷",
+    };
+
+    if (this.operator === null || this.resetOnNextInput === true) {
+      this.operator = operatorSymbols[operator];
+      this.display.upperText += operatorSymbols[operator];
+      this.display.updateUpper();
+      console.log(this.operator);
+    }
+  },
+
+  handelOperations: {
+    addition() {
+      calculator.accumulator += +calculator.currentInput;
+    },
+  },
+
   delete() {
     if (this.currentInput === "0") return;
     this.currentInput = this.currentInput.slice(0, -1) || "0";
     this.display.updateLower();
+  },
+
+  addition() {
+    this.handelOperations.addition();
+    this.display.upperText = this.accumulator;
+    this.display.updateUpper();
+    this.resetOnNextInput = true;
   },
 
   init() {
@@ -40,6 +81,11 @@ const calculator = {
         console.log("clicked " + number);
       } else if (operator === "delete") {
         this.delete();
+        console.log("clicked " + operator);
+      } else if (operator === "addition") {
+        this.addition();
+        this.handelOperator(operator);
+        console.log("clicked " + operator);
       }
     });
   },
